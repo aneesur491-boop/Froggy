174921ac12c795bef9414b63b4f3f5fa55d14f0d
aneesur491-boop <aneesur491@gmail.com>
2026-03-13 22:00:47 +0400

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    alert("Welcome to your HTML APK App!");
+}
+
+function changeText() {
+    document.getElementById("message").innerText =
+        "You clicked the button. Your app is working!";
+}
