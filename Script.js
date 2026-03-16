c400b7bda4689dff237a9fbd4b846860fa6be19b
aneesur491-boop <aneesur491@gmail.com>
2026-03-16 08:59:22 +0400

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
-function showMessage() {
-    alert("Welcome to your HTML APK App!");
-}
-
-function changeText() {
-    document.getElementById("message").innerText =
-        "You clicked the button. Your app is working!";
-}
